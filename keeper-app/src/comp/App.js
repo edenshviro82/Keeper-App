653c35c3b18d30f3f21c1ffb359680d3ab457d33
eden shviro <edenshviro82@gmail.com>
2022-11-20 17:46:01 +0200

    basic files

---
+++ keeper-app/src/comp/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import Note from "./Note"
+import React from 'react';
+
+function App(){
+
+    return <div>
+        <Header/>
+        <Note/>
+        <Footer/>
+    </div>;
+}
+
+export default App
---
+++ keeper-app/src/comp/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header(){
+
+    return <header> <h1>Eden's first react app</h1></header>
+
+}
+
+export default Header
